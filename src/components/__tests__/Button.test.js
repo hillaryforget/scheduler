609982533a41fdced1a1788a29757c8b5d669aeb
Hillary Forget <hillaryforget@outlook.com>
2023-01-27 13:06:00 -0500

    refactor Button using classnames

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -1,7 +1,6 @@
 import React from "react";
-
+import classNames from "classnames";
 import { render, cleanup, fireEvent } from "@testing-library/react";
-
 import Button from "components/Button";
 
 afterEach(cleanup);
