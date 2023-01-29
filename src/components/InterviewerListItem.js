f91e67c73d126ef09ea0b167b94764f6123cafb3
Hillary Forget <hillaryforget@outlook.com>
2023-01-29 18:24:13 -0500

    build InterviewerListItem.js component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,18 +1,24 @@
 import React from "react";
 import classNames from "classnames";
-import "component/InterviewerListItem.scss"
+import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
-
-}
+  
+  const interviewerClass = classNames('interviewers__item', {
+    'interviewers__item--selected': props.selected
+  });
 
   return (
-    <li className="interviewers__item">
-  <img
-    className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
-  />
-  Sylvia Palmer
-</li>
+    <li 
+      onClick={props.setInterviewer}
+      className={interviewerClass}
+      >
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
   );
+}
