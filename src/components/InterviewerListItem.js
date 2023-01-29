cb7868289d606f7ed4d3456d6e4f6f1cffa48ad9
Hillary Forget <hillaryforget@outlook.com>
2023-01-29 14:09:37 -0500

    create InterviewerListItem.js and scss files

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classNames from "classnames";
+import "component/InterviewerListItem.scss"
+
+export default function InterviewerListItem(props) {
+
+}
+
+  return (
+    <li className="interviewers__item">
+  <img
+    className="interviewers__item-image"
+    src="https://i.imgur.com/LpaY82x.png"
+    alt="Sylvia Palmer"
+  />
+  Sylvia Palmer
+</li>
+  );
