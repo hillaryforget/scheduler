608f53c9059eacca8088eb8b18d56da9f3789176
Hillary Forget <hillaryforget@outlook.com>
2023-01-30 13:57:13 -0500

    refactor List components so no longer passing id

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -9,10 +9,7 @@ export default function InterviewerListItem(props) {
   });
 
   return (
-    <li 
-      onClick={props.setInterviewer}
-      className={interviewerClass}
-      >
+    <li className={interviewerClass} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
