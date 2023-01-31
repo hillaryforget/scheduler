d8aab4b26a368d24397d29823ef951b3e413e26e
Hillary Forget <hillaryforget@outlook.com>
2023-01-31 03:04:45 -0500

    create folder for Appoitment component with files for views

--- stories/index.js
+++ stories/index.js
@@ -2,11 +2,20 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import "index.scss";
+
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem"
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+// import Show from "components/Appointment/Show";
+// import Confirm from "components/Appointment/Confirm";
+// import Status from "components/Appointment/Status";
+// import Error from "components/Appointment/Error";
+// import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -131,3 +140,12 @@ storiesOf("Button", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+  storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
