9514520c676baaa8c71cc78b0ee266451b256a81
Hillary Forget <hillaryforget@outlook.com>
2023-02-01 12:18:29 -0500

    add appointments data and implement .map and spread op

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "./Appointment";
 
 const days = [
   {
@@ -19,6 +20,45 @@ const days = [
     spots: 0,
   },
 ];
+
+const appointments = {
+  1: {
+    id: 1,
+    time: "12pm",
+  },
+  2: {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  3: {
+    id: 3,
+    time: "2pm",
+  },
+  4: {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      },
+    },
+  },
+  5: {
+    id: 5,
+    time: "4pm",
+  },
+};
 
 export default function Application(props) {
 
@@ -46,7 +86,9 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+      {Object.values(appointments).map((app) => {
+          return <Appointment key={app.id} {...app} />;
+        })}
       </section>
     </main>
   );
