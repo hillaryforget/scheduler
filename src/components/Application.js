4ba08934e9819680a40e98b4b3e187ea82b96f12
Hillary Forget <hillaryforget@outlook.com>
2023-02-05 14:25:59 -0500

    useEffect to GET days data from API so not hardcoded

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,68 +1,29 @@
-import React, { useState } from "react";
-import "components/Application.scss";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import "./Application.scss";
 import DayList from "components/DayList";
 import Appointment from "./Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
-const appointments = {
-  1: {
-    id: 1,
-    time: "12pm",
-  },
-  2: {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  3: {
-    id: 3,
-    time: "2pm",
-  },
-  4: {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      },
-    },
-  },
-  5: {
-    id: 5,
-    time: "4pm",
-  },
-};
-
 export default function Application(props) {
+  
+  const [selectedDay, setSelectedDay] = useState("Monday"); 
+  const [days, setDays] = useState([]);
 
-  const [day, setDay] = useState("Monday");
+  const [appointments, setAppointments] = useState([]);
+  const [interviewers, setInterviewers] = useState([]);
+  useEffect( () => {
+    Promise.all([
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers')
+    ]).then((all) => {
+      setDays(all[0].data);
+      setAppointments(all[1].data);
+      setInterviewers(all[2].data);
+    }).catch( (err) => {
+      console.log(err.message);
+    });
+  }, [selectedDay]);
 
   return (
     <main className="layout">
@@ -76,8 +37,8 @@ export default function Application(props) {
 <nav className="sidebar__menu">
   <DayList
   days={days}
-  value={day}
-  onChange={setDay} />
+  value={selectedDay}
+  onChange={setSelectedDay} />
 </nav>
 <img
   className="sidebar__lhl sidebar--centered"
--- src/components/DayList.js
+++ src/components/DayList.js
@@ -12,7 +12,9 @@ export default function DayList(props) {
         name ={day.name}
         spots ={day.spots}
         selected ={day.name === props.value}
-        setDay ={props.onChange}
+        setDay ={() => {
+          props.onChange(day.name);
+        }}
       />
     );
   });
