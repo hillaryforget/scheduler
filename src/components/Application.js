5c33cafad55f9bb04b5b0b806e2cf9381b8ca990
Hillary Forget <hillaryforget@outlook.com>
2023-02-07 03:03:25 -0500

    combine  states in Application to a single object

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,57 +1,59 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 import "./Application.scss";
 import DayList from "components/DayList";
 import Appointment from "./Appointment";
 
 export default function Application(props) {
-  
   const [state, setState] = useState({
     day: "Monday",
     interviewer: 2,
     days: [],
     appointments: {},
-    interviewers: []    
+    interviewers: [],
   });
 
-  const setDay = day => setState({ ...state, day });
-  const setDays = day => setState({ ...state, days });
+  const setDay = (day) => setState({ ...state, day });
 
-  useEffect( () => {
+  useEffect(() => {
     Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments'),
-      axios.get('http://localhost:8001/api/interviewers')
-    ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data,interviewers: all[2].data}));
-    }).catch( (err) => {
-      console.log(err.message);
-    });
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers"),
+    ])
+      .then((all) => {
+        setState((prev) => ({
+          ...prev,
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data,
+        }));
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   }, []);
 
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-  <DayList
-  days={state.days}
-  value={state.day}
-  onChange={setDay} />
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList days={state.days} value={state.day} onChange={setDay} />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-      {Object.values(appointments).map((app) => {
+        {Object.values(state.appointments).map((app) => {
           return <Appointment key={app.id} {...app} />;
         })}
       </section>
