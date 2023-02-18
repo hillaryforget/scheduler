5f591565097dbf31134f0d9ecfe2a9b95c1152e0
Hillary Forget <hillaryforget@outlook.com>
2023-02-18 15:22:44 -0500

    pass second test (getTestId)

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,27 +1,28 @@
 import React from "react";
-import { render, cleanup, getByPlaceholderText, getByTestId } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 import Form from "components/Appointment/Form";
 
 afterEach(cleanup);
 
 describe("Form", () => {
-const interviewers = [
-{
-  id: 1,
-  student: "Sylvia Palmer",
-  avatar: "https://i.imgur.com/LpaY82x.png"
-}
-];
+  const interviewers = [
+    {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png",
+}]
 
-const { getByPlaceholderText } = render(
-  <Form interviewers={interviewers} />
-);
+  it("renders without student name if not provided", () => {
+    const { getByPlaceholderText } = render(
+      <Form interviewers={interviewers} />
+    );
+    expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
+  });
 
-it("renders without student name if not provided", () => {
-  expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
-});
-
-// it("renders with initial student name", () => {
-//   expect(getByTestId("Student-name-input")).toHaveValue("Lydia Miller-Jones");
-//   });
+  it("renders with initial student name", () => {
+    const { getByTestId } = render(
+      <Form interviewers={interviewers} student="Lydia Miller-Jones" />
+    );
+    expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
+  })
 });
