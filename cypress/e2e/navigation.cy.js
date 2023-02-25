f705c9106fdc67cfb4c584130996980c46d6c1b9
Hillary Forget <hillaryforget@outlook.com>
2023-02-24 21:21:47 -0500

    add testid to navigation

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -5,12 +5,9 @@ describe("Navigation", () => {
 
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-    
-    cy.get("li").contains( "Tuesday").click();
-    cy.contains("li", "Tuesday").should("have.class", "day-list__item--selected");
-
-    cy.contains("li", "Tuesday")
-    .click()
-    .should("have.css", "background-color", "rgb(242, 242, 242)");
+  
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected")
   });
 });
