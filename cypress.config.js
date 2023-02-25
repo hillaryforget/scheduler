2016311a18fed95ac3305a6b1027ca0d741e20cb
Hillary Forget <hillaryforget@outlook.com>
2023-02-24 21:15:32 -0500

    adding cypress and navigating days

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  projectId: 'u7wkbt',
+  viewportWidth: 1280,
+  viewportHeight: 720,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:8000',
+  },
+})
--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,14 +4,14 @@ import DayListItem from "./DayListItem";
 export default function DayList(props) {
 
   const days = props.days.map((day) => {
-
+    console.log(day.name, props.day)
     return (
       <DayListItem
         key ={day.id}
         id ={day.id}
         name ={day.name}
         spots ={day.spots}
-        selected ={day.name === props.value}
+        selected ={day.name === props.day}
         setDay ={() => {
           props.setDay(day.name);
         }}
